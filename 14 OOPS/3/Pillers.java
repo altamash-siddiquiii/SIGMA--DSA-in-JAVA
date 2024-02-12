3a7fad6f94558a4e28765209197584f2862912fd
Altamash Siddiqui <altamashsiddiqui941@gmail.com>
2024-02-13 01:54:16 +0530

    Update code

--- 14 OOPS/3/Pillers.java
+++ 14 OOPS/3/Pillers.java
@@ -1,11 +1,26 @@
 public class Pillers {
     public static void main(String[] args) {
-        /* Pillers of Java
-
-        * (i) Encapsulation:- It is defined as the wrapping up of data and methods under a single unit/entity. It also implements data hiding. For example: When we creates a class which is known as entity and we declares its properties and functions
-        * (ii) Inheritance:-
-        * (iii) Polymorphism:-
-        * (iv) Abstraction:-
-        */
+        // Pillers of Java
+        // (i) Encapsulation (ii) Inheritance (iii) Polymorphism (iv) Abstraction
+        
+        Fish shark = new Fish();
+        shark.eat();
+    }
+    // Base Class (Parent class)
+    class Animal {
+        String color; // property
+        void eat() { // function
+            System.out.println("They eats...");
+        }
+        void breath() { // function
+            System.out.println("They breaths...");
+        }
+    }
+    // Derived Class (Child class)
+    class Fish extends Animal {
+        int fins; // property
+        void swim() { // function
+            System.out.println("They swims...");
+        }
     }
 }
