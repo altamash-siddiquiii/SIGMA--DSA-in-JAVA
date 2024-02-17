e09ecd7d12d3b4ad188d82727f4a641a76ec58a9
Altamash Siddiqui <altamashsiddiqui941@gmail.com>
2024-02-17 13:01:05 +0530

    Updated code of Abstract Classes

--- 14 OOPS/4 Pillers - iii/Pillers2.java
+++ 14 OOPS/4 Pillers - iii/Pillers2.java
@@ -1,12 +1,53 @@
-package 14 OOPS.4 Pillers - iii;
-
 public class Pillers2 {
     public static void main(String[] args) {
-        // Abstraction - inheritance
+        // Abstraction
         // (i) Abstract classes
+        // Animal a = new Animal(); // Cannot create object of abstract class
+        Horse h = new Horse();
+        h.eat(); h.walk(); System.out.println(h.color);
+        Chicken c = new Chicken();
+        c.eat(); c.walk(); System.out.println(c.color);
+        Mustang horse = new Mustang();
+        // Animal -> Horse -> Mustang (Hierarchae constructor is calling line by line)
     }
 }
-// Abstraction - inheritance
+// Abstraction
 // (I) ABSTRACT CLASSES
 abstract class Animal {
+    String color;
+    Animal() { // Can't create object but can create a constructor of abstract class
+        color = "Brown.";
+        System.out.println("Animal constructor is called...");
+    }
+    void eat() { // Non abstract method/function
+        System.out.println("Animal eats.");
+    }
+    abstract void walk(); // Abstract method/function
+}
+class Horse extends Animal {
+    Horse() {
+        System.out.println("Horse constructor is called...");
+    }
+    void changeColor() {
+        color = "Dark Brown.";
+    }
+    void walk() { // Compulsary to create method here of it's parent's class
+        System.out.println("Walks on 4 legs.");
+    }
+}
+class Mustang extends Horse {
+    Mustang() {
+        System.out.println("Mustang constructor is called...");
+    }
+}
+class Chicken extends Animal {
+    Chicken() {
+        System.out.println("Chicken constructor is called...");
+    }
+    void changeColor() {
+        color = "Yellow.";
+    }
+    void walk() { // Compulsary to create method here of it's parent's class
+        System.out.println("Walks on 2 legs.");
+    }
 }
