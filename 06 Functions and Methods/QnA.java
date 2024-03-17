dd4ab3848bd0b828dbe2e038d3076b0fbca51fc4
Altamash Siddiqui <altamashsiddiqui941@gmail.com>
2024-03-17 17:23:15 +0530

    QnA code updated

--- 06 Functions and Methods/QnA.java
+++ 06 Functions and Methods/QnA.java
@@ -10,6 +10,32 @@ public class QnA {
             return false;
         }
     }
+    public static boolean isPalindrome(int num) {
+        int originalNum = num;
+        int reverseNum = 0;
+        
+        while (num != 0) {
+            int remainder = num % 10;
+            reverseNum = reverseNum * 10 + remainder;
+            num = num / 10;
+        }
+        // if original number and reverse number is equal or not
+        if (originalNum == reverseNum) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public static int sumOfDigits(int num) {
+        int sum = 0;
+
+        while (num > 0) {
+            int lastDigit = num % 10;
+            sum += lastDigit;
+            num /= 10;
+        }
+        return sum;
+    }
     public static void main(String[] args) {
         
         // Question 1 and His Solution
@@ -29,13 +55,34 @@ public class QnA {
         System.out.println("Average of integers is : " + averageIs); */
 
         // Question 2 and His Solution
-        Scanner sc = new Scanner(System.in);
+        /* Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number : ");
         int num = sc.nextInt();
         if (isEven(num)) {
             System.out.println("Number is EVEN");
         } else {
             System.out.println("Number is ODD");
-        }
+        } */
+
+        // Question 3 and His Solution
+        /* System.out.print("Enter a number : ");
+        Scanner sc = new Scanner(System.in);
+        int palindromeNum = sc.nextInt();
+        if (isPalindrome(palindromeNum)) {
+            System.out.println(palindromeNum + " is a Palindrome");
+        } else {
+            System.out.println(palindromeNum + " is not a Palindrome");
+        } */
+
+        // Question 4 and His Solution
+        /* READ AND CODE EXERCISE
+        * Search about (on Google) & use the following methods of the Math class in Java:
+        * (i) Math.min( ) (ii) Math.max( ) (iii) Math.sqrt( )
+        * (iv) Math.pow( ) (v) Math.avg( ) (vi) Math.abs( )
+        * Free reading resource ( https://www.javatpoint.com/java-math )  */
+
+        // Question 5 and His Solution
+        int sod = sumOfDigits(456);
+        System.out.println("Sum of Digits : " + sod);
     }
 }
