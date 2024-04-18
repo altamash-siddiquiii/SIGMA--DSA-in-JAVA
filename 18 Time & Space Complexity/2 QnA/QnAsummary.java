6628ddb02d30bbdc3cf5a253f606f53db18669c1
Altamash Siddiqui <altamashsiddiqui941@gmail.com>
2024-04-18 17:14:09 +0530

    Time and Space Complexity code is ended.

---
+++ 18 Time & Space Complexity/2 QnA/QnAsummary.java
@@ -0,0 +1,5 @@
+public class QnAsummary {
+    public static void main(String[] args) {
+        System.out.print("Some Questions related with 'Time and Space Complexity' is given in the PDF form with Solutions.");
+    }
+}
